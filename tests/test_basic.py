eaed5e27e5bea154e450c5a32cbc37abee480242
Jairo Llopis <yajo.sk8@gmail.com>
2023-01-13 11:52:40 +0000

    feat: support kwargs
    This gives more freedom to use the extension.
    
    For example, as seen in the added test, you can pass some input to the command, which can then use it as its STDIN value.
    
    @moduon MT-1075

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -12,3 +12,9 @@ def test_extension1():
     template = env.from_string("test {{ \"whoami\"|shell }}")
     result = template.render()
     assert len(result) >= 6
+
+def test_input():
+    env = Environment(extensions=["jinja2_shell_extension.ShellExtension"])
+    template = env.from_string("{{ 'cat' | shell(input='meow', text=true) }}")
+    result = template.render()
+    assert result == "meow"
