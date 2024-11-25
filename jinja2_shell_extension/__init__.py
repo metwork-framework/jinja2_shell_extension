e7b202d320633156d8ddc5921ccd99797293e2ab
Jean-Baptiste VESLIN <19856429+thebaptiste@users.noreply.github.com>
2024-11-25 11:21:20 +0100

    fix: Windows compatibility
    Closes: #13

--- jinja2_shell_extension/__init__.py
+++ jinja2_shell_extension/__init__.py
@@ -13,7 +13,7 @@ def shell(eval_ctx, value, die_on_error=False, encoding="utf8", **kwargs):
     if die_on_error:
         cmd = value
     else:
-        cmd = "%s ; exit 0" % value
+        cmd = "%s || exit 0"
     return subprocess.check_output(cmd, stderr=subprocess.STDOUT, shell=True, encoding=encoding, **kwargs)
 
 
